7eca75f75ef9b0b0271f67e093c83ba677dbfc1b
cpnsn <cpoinson@hotmail.fr>
2023-10-30 23:08:50 +0100

    step 4

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,42 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+async function getPhotographers() {
+    try {
+        const response = await fetch('data/photographers.json');
+        const data = await response.json();
+        console.log(data);
+        return data.photographers;
+    } catch (error) {
+        console.error('error fetching data', error);
+        return [];
+    };
+}
+
+const params = new URLSearchParams(window.location.search);
+const photographerId = params.get('id');
+
+
+async function displayData(photographer) {
+    const photographHeader = document.querySelector(".photograph-header");
+
+    const h2 = document.createElement('h2');
+    h2.textContent = photographer.name;
+    
+    const location = document.createElement('p');
+    location.textContent = `${photographer.city}, ${photographer.country}`;
+    
+    photographHeader.appendChild(h2);
+    photographHeader.appendChild(location);
+
+    return (photographHeader);
+}
+
+
+async function init() {
+    const photographers = await getPhotographers();
+    const photographer = photographers.find(photographer => photographer.name === photographerId);
+    displayData(photographer);
+}
+
+init();
+
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,44 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute("aria-label", `${name} details`)
+
+        const a = document.createElement('a');
+        a.setAttribute("href", `photographer.html?id=${name}`);
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", name)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        a.appendChild(img);
+        a.appendChild(h2);
+
+        const p1 = document.createElement( 'p' );
+        p1.textContent = city + ', ' + country;
+        p1.setAttribute("id", "location");
+
+        const p2 = document.createElement( 'p' );
+        p2.textContent = tagline;
+        p2.setAttribute("id", "tagline");
+
+        const p3 = document.createElement( 'p' );
+        p3.textContent = price + '€/jour';
+        p3.setAttribute("id", "price");
+
+        // article.appendChild(img);
+        // article.appendChild(h2);
+        article.appendChild(a);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(p3);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
